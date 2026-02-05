677f990204b3f35136ab5a497625173534078313
sage <jjhcat0402@gmail.com>
2026-02-05 16:37:12 +0900

    Update vuln.c

--- bof-rop-basic/vuln.c
+++ bof-rop-basic/vuln.c
@@ -1,40 +1,46 @@
 // filename: vuln.c
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
-const char *binsh = "/bin/sh";
+char stage[0x1000];
 
-__attribute__((noinline, used))
-void ROP_bank(void) {
-    __asm__ volatile(
-        "pop %eax; ret;\n"
-        "pop %ebx; ret;\n"
-        "pop %ecx; ret;\n"
-        "pop %edx; ret;\n"
+__attribute__((naked))
+void syscall_gadget() {
+    __asm__ __volatile__(
+        "int $0x80;\n"
+        "ret;\n"
     );
 }
 
-__attribute__((noinline, used))
-void syscall_stub(void) {
-    __asm__ volatile("int $0x80; ret;");
+__attribute__((used))
+void trash1(int a, int b, int c) {
+    volatile int x = a + b + c;
+    (void)x;
 }
 
-__attribute__((noinline, used))
-void do_setregid(void) {
-    gid_t egid = getegid();
-    setregid(egid, egid);
+__attribute__((used))
+void trash2(int a, int b, int c) {
+    volatile int x = b;
+    volatile int y = a;
+    (void)x; (void)y; (void)c;
 }
 
-void vuln(void) {
+__attribute__((used))
+void trash3(int a, int b, int c) {
+    volatile int x = 0;
+    if (a == 0xdeadbeef) x = b;
+    (void)x; (void)c;
+}
+
+void vuln() {
     char buf[64];
-    puts("input:");
-    gets(buf);
+    puts("stage1:");
+    read(0, buf, 400);
 }
 
-int main(void) {
+int main() {
     setvbuf(stdout, NULL, _IONBF, 0);
+    setregid(getegid(), getegid());
     vuln();
-    puts("done");
     return 0;
 }
