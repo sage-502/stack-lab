92bd648cfb69fc8198d15e39684f2c5a387bd240
sage <jjhcat0402@gmail.com>
2026-02-09 15:29:57 +0900

    Update and rename vuln.c to vuln.c

---
+++ bof-rop-pivot/vuln.c
@@ -0,0 +1,41 @@
+// filename: vuln.c
+#include <stdio.h>
+#include <unistd.h>
+
+char stage[0x1000];
+
+__attribute__((naked))
+void syscall_gadget() {
+    __asm__ volatile(
+        "int $0x80;\n"
+        "ret;\n"
+    );
+}
+
+__attribute__((noinline, used))
+void ROP_bank(void) {
+    __asm__ volatile(
+        "pop %eax; ret;\n"
+        "pop %ebx; ret;\n"
+        "pop %ecx; ret;\n"
+        "pop %edx; ret;\n"
+    );
+}
+
+__attribute__((noinline, used))
+void do_setregid(void) {
+    gid_t egid = getegid();
+    setregid(egid, egid);
+}
+
+void vuln() {
+    char buf[64];
+    puts("input:");
+    read(0, buf, 400);
+}
+
+int main() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    vuln();
+    return 0;
+}
