65c95b42809529426df3f490b610c7b64f76f718
sage <jjhcat0402@gmail.com>
2026-02-11 01:17:03 +0900

    Create fix.c

---
+++ bof-rop-pivot/fix.c
@@ -0,0 +1,42 @@
+// filename: fix.c
+#include <stdio.h>
+#include <unistd.h>
+
+char stage[0x1000];
+
+__attribute__((naked))
+void syscall_gadget() {
+    __asm__ volatile(
+        "int $0x80;\n"
+        "ret;\n"
+    );
+}
+
+__attribute__((noinline, used))
+void ROP_bank(void) {
+    __asm__ volatile(
+        "pop %eax; ret;\n"
+        "pop %ebx; ret;\n"
+        "pop %ecx; ret;\n"
+        "pop %edx; ret;\n"
+        "mov %eax, %ebx; ret;\n"
+        "mov %eax, %ecx; ret;\n"
+    );
+}
+
+void vuln() {
+    char buf[64];
+    puts("input:");
+    ssize_t n = read(0, buf, sizeof(buf) - 1);
+  
+    if (n <= 0)
+        return;
+
+    buf[n] = '\0';
+}
+
+int main() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    vuln();
+    return 0;
+}
